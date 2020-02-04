e36a742c21c8b0d6b558363d0c8c3c06a55f3ce3
AlundraOz <ailuozols@gmail.com>
2020-02-04 16:22:02 +0100

    first try

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const name = "Alundra Ozono";
+const avatar = "https://scontent.fbcn9-1.fna.fbcdn.net/v/t1.0-9/p720x720/40056635_545439302556318_6961266852869701632_o.jpg?_nc_cat=111&_nc_ohc=eC7GZUAvSWcAX_iDeVr&_nc_ht=scontent.fbcn9-1.fna&_nc_tp=6&oh=66da40d7285d2b0e38ec582f01d8726f&oe=5ECD98BC";
+const online = true;
+
+function Contact() {
+  <div class="contact">
+    <img src={avatar} class="avatar"/>
+    <div class="about">
+      <div class="name"><h4>{name}</h4></div>
+            <div class="status">
+              <i class={classStatus}></i>{statusText}
+            </div>
+          </div>
+  </div>
+}
+
+ReactDOM.render(element, document.getElementById('root'));
+
+export default Contact
