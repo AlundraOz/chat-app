abf1fd6a0fd1dcf850aa6fda8172b0e05a442ad0
AlundraOz <ailuozols@gmail.com>
2020-02-05 11:20:19 +0100

    added props and condition boolean

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const name = "Alundra Ozono";
-const avatar = "https://scontent.fbcn9-1.fna.fbcdn.net/v/t1.0-9/p720x720/40056635_545439302556318_6961266852869701632_o.jpg?_nc_cat=111&_nc_ohc=eC7GZUAvSWcAX_iDeVr&_nc_ht=scontent.fbcn9-1.fna&_nc_tp=6&oh=66da40d7285d2b0e38ec582f01d8726f&oe=5ECD98BC";
-const online = true;
 
-function Contact() {
-  <div class="contact">
-    <img src={avatar} class="avatar"/>
-    <div class="about">
-      <div class="name"><h4>{name}</h4></div>
-            <div class="status">
-              
-            </div>
-          </div>
-  </div>
+function Contact(props) {  
+    
+    return (<div className="Contact">
+    <h1 className="name">{props.name}</h1>
+    <img className="avatar" src={props.image}/>
+    {
+        props.status === true
+            ? <h3 className="status-online">Online</h3>
+            : <h3 className="status-offline">Offline</h3> 
+    }
+    <h3 className="status">{props.status}</h3>
+    </div>)
+
 }
 
 export default Contact
